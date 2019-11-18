c692d73501200f196329f4a20d36695af0bb9dfb
duvld <ollejna@gmail.com>
2019-11-18 12:47:53 -0500

    WIP: added new password reset form getting __init__ error

--- accounts/views.py
+++ accounts/views.py
@@ -5,9 +5,16 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from accounts.forms import PasswordResetFormWithUsername
 
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+class PasswordResetFormWithUsernameView(generic.CreateView):
+	form_class = PasswordResetFormWithUsername
+	success_url = reverse_lazy('login')
+	template_name = 'signup.html'
+	
